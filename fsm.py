a08a7f9785fb85f92d7f3b833ce99732b2dbc6d1
b112137 <b112137@yahoo.com.tw>
2019-11-24 22:04:36 +0800

    test

--- fsm.py
+++ fsm.py
@@ -47,4 +47,9 @@ class TocMachine(GraphMachine):
         self.go_back()
 
     def on_exit_state3(self):
+        print("Leaving state3")
+
+    def on_exit_user(self):
         print("Leaving user")
+
+        
