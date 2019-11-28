d473080cb0704e6dbcb70f0175b30d995c8c3b8d
b112137 <b112137@yahoo.com.tw>
2019-11-28 11:13:15 +0800

    add states

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import os
+import requests
 
 from linebot import LineBotApi, WebhookParser
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
@@ -13,7 +14,28 @@ def send_text_message(reply_token, text):
 
     return "OK"
 
+def send_button_message(id, text, btn):
+    url = "{0}/me/messages?access_token={1}".format(GRAPH_URL, ACCESS_TOKEN)
+    aaa = {
+      "recipient": {"id": id},
+      "message":{
+        "attachment":{
+        "type":"template",
+        "payload":{
+          "template_type":"button",
+          "text":text,
+          "buttons":btn
+        }
+      }
+    }
+  }
+    response = requests.post(url, json=aaa)
 
+    if response.status_code != 200:
+        print("Unable to send message: " + response.text)
+    return response
+
+    
 """
 def send_image_url(id, img_url):
     pass
